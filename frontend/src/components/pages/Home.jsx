b6a63ce55e5eb1299d0e34a5d2b88483b5503e27
unknown <naimcse56@gmail.com>
2025-09-27 19:32:52 +0600

    Home Page UI

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Layout from '../common/Layout'
+import Hero from '../common/Hero'
+import FeaturedCategories from '../common/FeaturedCategories'
+import FeaturedCourses from '../common/FeaturedCourses'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Layout>
+      <Hero/>
+      <FeaturedCategories/>
+      <FeaturedCourses/>
+    </Layout>
   )
 }
 
